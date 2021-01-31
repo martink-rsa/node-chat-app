4d45d1a9fcf8c3d2647de8cf1346e7d030164068
Martin Kruger <martin@martinkruger.com>
2021-01-31 12:28:01 +0200

    Acknowledgements added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,36 @@
-io();
+const socket = io();
+
+socket.on('message', (message) => {
+  console.log('Message:', message);
+});
+
+document.querySelector('#chat-message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = e.target.elements['chat-message'].value;
+
+  socket.emit('sendMessage', message, (error) => {
+    // This callback is running when after the message was acknowledged on the
+    //    backend and it's own callback has run
+    // You can add more than own parameter into the 'sendMessage' emitter but the cb
+    // must be the last in the list e.g.
+    //    'sendMessage', message, arg2, arg3, (cbMessage) =>..
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message delivered');
+  });
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Your browser does not support geolocation');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    const location = { latitude, longitude };
+    socket.emit('sendLocation', location, () => {
+      console.log('Location shared');
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,9 +14,48 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
-  //
+io.on('connection', (socket) => {
+  // First argument ('message') is name of event that is being emitted
+  // Second parameter ('Welcome!') is data being emitted
+  socket.emit('message', 'Welcome!');
   console.log('New WebSocket connection');
+
+  // broadcast.emit will send the data to all clients except the
+  //    client that triggered it
+  socket.broadcast.emit('message', 'A new user has joined');
+
+  // Listen to the 'sendMessage' event that is being
+  //    emitted by the front end. This message is then sent
+  //    to all connected browsers using io.emit
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed');
+    }
+
+    io.emit('message', message);
+    // The callback will run when the message was delivered
+    //    from the frontend script (sendMessage emitter)
+    // It will send the arguments back to the sendMessage emitter
+    //    which will be available in the callback there
+    callback();
+  });
+
+  socket.on('sendLocation', (location, callback) => {
+    const { latitude, longitude } = location;
+
+    io.emit(
+      'message',
+      `User location: https://google.com/maps?q=${latitude},${longitude}`,
+    );
+    callback();
+  });
+
+  // Send a message when a user disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has disconnected');
+  });
 });
 
 server.listen(port, () => {
